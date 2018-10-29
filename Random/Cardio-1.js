cdd9b44865b1acf875e605dd07aec2d1506deeaa
bayoishola20 <bayoishola20@yahoo.com>
2018-10-29 09:27:37 +0100

    Cardio

--- Random/Cardio-1.js
+++ Random/Cardio-1.js
@@ -1,9 +1,69 @@
 // REVERSE STRING
 
-function reverseString(str) {
+/* ================== Method A ================== */
+
+function reverseStringA(str) {
     return str.split('').reverse().join('');
 }
 
-const reverse = reverseString('bayo');
+const reverseA = reverseStringA('bayo');
 
-console.log(`reverse string is "${reverse}"`);
+console.log(`reverse string is "${reverseA}"`);
+
+/* ================== Method B ================== */
+
+function reverseStringB(str) {
+    let revString ='';
+    for(let i = 0; i <= str.length - 1; i++) {
+        revString = str[i] + revString; // revString + str[i] will not reverse string accordingly
+    }
+    return revString;
+}
+
+const reverseB = reverseStringB('hello');
+
+console.log(`reverse string is "${reverseB}"`);
+
+/* ================== Method C ================== */
+
+function reverseStringC(str) {
+    let revString ='';
+    for(let i of str) {
+        revString = i + revString;
+    }
+    return revString;
+}
+
+const reverseC = reverseStringC('common');
+
+console.log(`reverse string is "${reverseC}"`);
+
+/* ================== Method D ================== */
+
+function reverseStringD(str) {
+    let revString = ''
+    str.split('').forEach( (i) => {
+        revString = i + revString;
+    })
+    return revString;
+}
+
+const reverseD = reverseStringD('nigeria');
+
+console.log(`reverse string is "${reverseD}"`);
+
+/* ================== Method E ================== */
+
+function reverseStringE(str) {
+    return str.split('').reduce( (revString, i) => i + revString, '');
+}
+
+const reverseE = reverseStringE('former');
+
+console.log(`reverse string is "${reverseE}"`);
+
+
+
+// PALINDROME
+
+
